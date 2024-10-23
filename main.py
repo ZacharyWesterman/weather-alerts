7e8cd5af59f32b744a0650947d020ae16b5d6c5c
Zachary Westerman <westerman.zachary@gmail.com>
2024-10-23 18:28:55 -0500

    Remove excess blank line in alert msg

--- main.py
+++ main.py
@@ -31,10 +31,10 @@ if __name__ == '__main__':
 				msg = ''
 				if len(below):
 					plural = '' if len(below) == 1 else 's'
-					msg += f'\nLow{plural} at or below {min}F on:\n' + '\n'.join(below)
+					msg += f'Low{plural} at or below {min}F on:\n' + '\n'.join(below)
 				if len(above):
 					plural = '' if len(above) == 1 else 's'
-					msg += f'\nHigh{plural} at or above {max}F on:\n' + '\n'.join(above)
+					msg += f'High{plural} at or above {max}F on:\n' + '\n'.join(above)
 				msg += '\nChecking local forecasts is recommended.'
 
 				if not DRY_RUN:
