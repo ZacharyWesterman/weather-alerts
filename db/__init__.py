b4a268601e59e072902bca2a50c28d5d2708b42c
Zachary Westerman <westerman.zachary@gmail.com>
2025-06-14 15:30:49 -0500

    Remove vestigial references to phone numbers : closes #2

--- db/__init__.py
+++ db/__init__.py
@@ -1,21 +1,8 @@
-__all__ = ['log', 'create_user']
+__all__ = ['log']
 
 import re
 import skrunk_api
 from app import credentials
-
-def create_user(*, id: str, lat: float, lon: float, phone: str, exclude: bool = False) -> None:
-	phone = re.sub('[^0-9]', '', phone)
-	if len(phone) < 11:
-		raise Exception(f'ERROR: Invalid phone number "+{phone}"')
-
-	cfg = {
-		'_id': id,
-		'lat': lat,
-		'lon': lon,
-		'phone': f'+{phone}',
-		'exclude': exclude,
-	}
 
 def log(sent_list: list, error: str) -> None:
 	cred = credentials.get('skrunk_api')
